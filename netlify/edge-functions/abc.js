e6d32aab7179bf2b8dd9503a71394041c4e6fbff
Yujohn Nattrass <yujohn.natt@gmail.com>
2024-11-17 13:36:50 -0800

    fix

--- netlify/edge-functions/abc.js
+++ netlify/edge-functions/abc.js
@@ -2,7 +2,6 @@ import { HTMLRewriter } from "https://deno.land/x/html_rewriter@v0.1.0-pre.17/in
 const handler = async (request, context) => {
   const res = await context.next();
   const rewriter = new HTMLRewriter();
-  rewriter.trans;
   return await new HTMLRewriter().transform(res);
 };
 
